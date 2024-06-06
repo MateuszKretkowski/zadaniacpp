d855695721328f81a5f7e5e2ddc45eedbd4cca9d
Mateusz Kretkowski <mkretkowski13@gmail.com>
2024-06-06 12:37:16 +0200

    questionC: calculating bitwise AND operator and checking_thru_list

---
+++ zagadkibitowe/Zbiory/questionC.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "checking_thru_list.h"
+using namespace std;
+
+int main() {
+        int n;
+        int m;
+        cout << "wpisz liczbe n: ";
+        cin >> n;
+        cout << "wpisz liczbe m: ";
+        cin >> m;
+        int liczba = n&m;
+        checking_thru_list(liczba);
+        return 0;
+}
