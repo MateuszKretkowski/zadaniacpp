0ee4449585eda85adb0e4e777074143e35334d00
Mateusz Kretkowski <mkretkowski13@gmail.com>
2024-06-09 14:55:43 +0200

    Sito Eratostelesa: finding amount of Prime numbers < n, in a matrix of n size.

--- zagadkibitowe/sitoeratostenesa.cpp
+++ zagadkibitowe/sitoeratostenesa.cpp
@@ -6,36 +6,39 @@ int main() {
         int n;
         cout << "podaj liczbe n < 10000: ";
         cin >> n;
-        int matrix [6][5];
+        int matrix [n/5][5];
         int counter=0;
         int count=2;
-        for (int d = 0; d < 6; d++) { 
+        for (int d = 0; d < n/5; d++) { 
                 for (int f = 0; f < 5; f++) { 
                         matrix[d][f] = count; 
                         count++; 
                 } 
         } 
         int id = 0;
+        int idforwhile=id;
         int last = -1;
         bool end=false;
-        while (id > last) {
+        int c=0;
+        while (c < n && last < n) {
         cout << "id na poczatek while: " << id << endl;
-                for (int d = 0; d < 6; d++) { 
+                for (int d = 0; d < n/5; d++) { 
                                 for (int f = 0; f < 5; f++) { 
-                                        cout << "odbywa sie!";
                                         cout << "LAST PRZED ZAMIANA: " << last << endl;
-                                        if (matrix[d][f] != 0 && id == 0 && matrix[d][f] != last) {
+                                        cout << "MATRIXDF: " << matrix[d][f] << endl;
+                                        if (matrix[d][f] != 0 && id == 0 && matrix[d][f] != last && matrix[d][f] > last) {
                                                 id = matrix[d][f];
                                                 last = id;
                                                 cout << "LAST: " << last << endl;
                                                 cout << "ID: " << id << endl;
                                                 break;
+                                                cout << "po breaku" << endl;
                                         }
-                                } 
+                }
                 }
                          
                         cout << "po petli" << endl;
-                        for (int i=0; i<6; i++) {
+                        for (int i=0; i<n/5; i++) {
                         for (int j=0; j<5; j++) {
                                 if (matrix[i][j] % id == 0 && matrix[i][j] != 0 && matrix[i][j] != id) {
                                         cout << "ustawiamy " << matrix[i][j] << "na zero" << endl;
@@ -44,21 +47,19 @@ int main() {
                         }
                         
                 }
-       
-        id = 0;
-        
-                        cout << "oto matrix: " << endl;
-        for (int d = 0; d < 6; d++) { 
+       id = 0;
+       c = c+1;
+        }        
+       cout << "oto matrix: " << endl;
+        for (int d = 0; d < n/5; d++) { 
                 cout << "| ";
                 for (int f = 0; f < 5; f++) { 
                         if (matrix[d][f] != 0 && matrix[d][f] < n) {
                                 counter = counter+1;
                         }
                         cout << matrix[d][f] << " * "; 
-                        count++; 
                 }
                 cout << " |" << endl;
-        }
         }
         cout << "Oto liczba liczb pierwszych ktore sa mniejsze od n: " << counter << endl;
        
