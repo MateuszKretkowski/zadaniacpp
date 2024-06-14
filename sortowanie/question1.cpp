69cf58bb609aa74a811fc3e84622b581d300c498
Mateusz Kretkowski <mkretkowski13@gmail.com>
2024-06-14 14:29:55 +0200

    Sortowanie: Question1.cpp: Quick Sort

--- sortowanie/question1.cpp
+++ sortowanie/question1.cpp
@@ -19,16 +19,50 @@ void bubblesort(int nums[], int n) {
                 }
         }
         
-        for (int k=0; k<n-1; k++) {
-                cout << k << ", ";
-        }
         cout << endl;
         cout << "Counter: " << counter << endl;
 }
 
+void change(int nums[], int p1, int p2) {
+    int temp = nums[p1];
+    nums[p1] = nums[p2];
+    nums[p2] = temp;
+}
+
+int dividenums(int nums[], int l, int r) {
+    int pivot = nums[r];
+    int i = l - 1;
+
+    for (int j = l; j < r; j++) {
+        if (nums[j] < pivot) {
+            i++;
+            change(nums, i, j);
+        }
+    }
+    change(nums, i + 1, r);
+    return i + 1;
+}
+
+void quicksort(int nums[], int l, int r) {
+    if (l < r) {
+        int pi = dividenums(nums, l, r);
+
+        quicksort(nums, l, pi - 1);
+        quicksort(nums, pi + 1, r);
+    }
+}
+
+
 int main() {
         int array[] = {1,5,4,7,2,6,3};
-        int N = sizeof(array) / sizeof(array[0]);
-        bubblesort(array, N);
+        for (int i : array) {
+                cout << " * " << i << endl;
+        }
+        int N = sizeof(array) / sizeof(array[0])-1;
+        cout << "SIZE OF ARRAY: " << N << endl;
+        quicksort(array, 0, N);
+        for (int i : array) {
+                cout << " * " << i << endl;
+        }
         return 0;
 }
